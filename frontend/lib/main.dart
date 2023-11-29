a643aa1601c8554f61e346e2a7fab0e3957aa8b1
Jake Coker <jake.coker91@gmail.com>
2023-11-29 13:41:15 -0800

    Created tdd flow for notes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:frontend/models/note.dart';
-import 'service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,63 +21,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Note> notes = [];
-  // Refreshes notes from the server
-  void updateNotes() {
-    Service.get('notes').then((response) {
-      final data = response.body;
-      return data;
-    }).then((data) {
-      final jsonData = jsonDecode(data) as List<dynamic>;
-      setState(() {
-        notes.clear();
-        for (var element in jsonData) {
-          notes.add(Note.fromJson(element as Map<String, dynamic>));
-        }
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    updateNotes();
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(notes[index].title),
-              subtitle: Text(notes[index].content),
-            );
-          },
-          itemCount: notes.length,
+        appBar: AppBar(
+          title: Text(
+            title,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => updateNotes(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: const Center(child: Text('Hello, world!')));
   }
 }
